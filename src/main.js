ab6461531fd9c3b6e8620264bdf2bc0bb522b9d8
ZigEliaZag <eliakaliza50@icloud.com>
2026-03-18 02:44:13 +0100

    comments to make code lines clear

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ const showMenu = document.querySelector("header .click-to-open");
 const navBar = document.querySelector("header .navbar-large-screen");
 
 function openMenu() {
+    // active menu button when click, nav rotate into X 
     showMenu.classList.toggle("active");
+    // show menu in navbar when click
     navBar.classList.toggle("open")
 
 }
 
 showMenu.addEventListener("click", openMenu);
-
-console.log("heeej")
